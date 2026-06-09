bad691f9d2d8c3feb1275221bd6a7b3e85d1b9bb
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2026-06-10 01:27:50 +0900

    feat: apply nimble css to root route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,22 +2,28 @@
 	import { resolve } from '$app/paths';
 </script>
 
-<ul>
-	<li><a href={resolve('/fractal-noise')}>fractal noise</a></li>
-	<li><a href={resolve('/static')}>static</a></li>
-	<li><a href={resolve('/double-static')}>double static</a></li>
-	<li><a href={resolve('/gradient')}>gradient</a></li>
-	<li><a href={resolve('/fire-plasma')}>fire plasma</a></li>
-</ul>
+<svelte:head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@leftium/nimble.css/dist/nimble.min.css" />
+</svelte:head>
 
-<ul>
-	<li><a href={resolve('/fire-palette')}>fire palette</a></li>
-</ul>
+<main>
+	<ul>
+		<li><a href={resolve('/fractal-noise')}>fractal noise</a></li>
+		<li><a href={resolve('/static')}>static</a></li>
+		<li><a href={resolve('/double-static')}>double static</a></li>
+		<li><a href={resolve('/gradient')}>gradient</a></li>
+		<li><a href={resolve('/fire-plasma')}>fire plasma</a></li>
+	</ul>
 
-Static effect based on:<a href="https://codepen.io/matthewhudson/pen/KOPxNv"
-	>https://codepen.io/matthewhudson/pen/KOPxNv</a
->
+	<ul>
+		<li><a href={resolve('/fire-palette')}>fire palette</a></li>
+	</ul>
 
-<hr />
+	Static effect based on:<a href="https://codepen.io/matthewhudson/pen/KOPxNv"
+		>https://codepen.io/matthewhudson/pen/KOPxNv</a
+	>
 
-<a href="https://github.com/Leftium/fx">https://github.com/Leftium/fx</a>
+	<hr />
+
+	<a href="https://github.com/Leftium/fx">https://github.com/Leftium/fx</a>
+</main>
